c99412bc88309e9c4dc0a3e1dab4772cd6de4b60
Wallysson <wallysson.mateus.barbosa@hotmail.com>
2023-06-10 13:41:26 -0300

    att

--- backend/src/routes/rotasUser.js
+++ backend/src/routes/rotasUser.js
@@ -4,7 +4,55 @@ const Usuario = require("../models/User");
 const Moeda = require("../models/Moeda");
 const jwt = require("jsonwebtoken");
 const gerarToken = require("../services/gerarToken");
-// const verificarToken = require("../../middleware/authMiddleware");
+
+const redis = require("redis");
+
+// Rota para listar todas as moedas com Redis cache
+router.get("/moedas", async (req, res) => {
+  const redisClient = redis.createClient({
+    host: "localhost",
+    port: 6379,
+  });
+
+  redisClient.on("error", (err) => {
+    console.error("Erro de conexão com o Redis:", err);
+  });
+
+  try {
+    const moedas = await new Promise((resolve, reject) => {
+      // Verifica se os dados estão no cache do Redis
+      redisClient.get("moedas", (err, reply) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else if (reply) {
+          // Se os dados estiverem no cache, retorna a resposta do cache
+          const moedas = JSON.parse(reply);
+          resolve(moedas);
+        } else {
+          // Se os dados não estiverem no cache, busca no banco de dados
+          Moeda.find()
+            .then((moedas) => {
+              // Armazena os dados no cache do Redis por 1 minuto (por exemplo)
+              redisClient.setex("moedas", 60, JSON.stringify(moedas));
+              resolve(moedas);
+            })
+            .catch((err) => {
+              console.error(err);
+              reject(err);
+            });
+        }
+      });
+    });
+
+    res.json(moedas);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao buscar moedas");
+  } finally {
+    redisClient.quit(); // Encerra o cliente Redis somente após a conclusão das operações
+  }
+});
 
 // Rota para gerar um token
 router.get("/token", (req, res) => {
@@ -54,27 +102,58 @@ router.post("/moedas", async (req, res) => {
   }
 });
 
-// Rota para listar todas as moedas
-router.get("/moedas", async (req, res) => {
-  try {
-    const moedas = await Moeda.find();
-    res.json(moedas);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Erro ao buscar moedas");
-  }
-});
+// // Rota para listar todas as moedas com Redis cache
+// router.get("/moedas", async (req, res) => {
+//   try {
+//     // Verifica se os dados estão no cache do Redis
+//     redisClient.get("moedas", async (err, reply) => {
+//       if (err) {
+//         console.error(err);
+//         res.status(500).send("Erro ao buscar moedas");
+//       } else if (reply) {
+//         // Se os dados estiverem no cache, retorna a resposta do cache
+//         const moedas = JSON.parse(reply);
+//         res.json(moedas);
+//       } else {
+//         // Se os dados não estiverem no cache, busca no banco de dados
+//         const moedas = await Moeda.find();
 
-// Rota para buscar uma moeda por nome
-router.get("/moedas/:nome", async (req, res) => {
-  try {
-    const nome = req.params.nome;
-    const moedas = await Moeda.find({ nome: nome }).exec();
-    res.json(moedas);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+//         // Armazena os dados no cache do Redis por 1 minuto (por exemplo)
+//         redisClient.setex("moedas", 60, JSON.stringify(moedas));
+
+//         res.json(moedas);
+//       }
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Erro ao buscar moedas");
+//   }
+// });
+
+// // Rota para buscar uma moeda por nome
+// router.get("/moedas/:nome", async (req, res) => {
+//   const nome = req.params.nome;
+
+//   // Verifica se a resposta está em cache
+//   redisClient.get(nome, async (err, cachedData) => {
+//     if (err) {
+//       console.error("Erro ao buscar dados em cache:", err);
+//     }
+
+//     if (cachedData) {
+//       console.log("Dados recuperados do cache");
+//       res.json(JSON.parse(cachedData));
+//     } else {
+//       try {
+//         const moedas = await Moeda.find({ nome: nome }).exec();
+//         redisClient.setex(nome, 3600, JSON.stringify(moedas)); // Armazena os dados em cache por 1 hora
+//         res.json(moedas);
+//       } catch (error) {
+//         res.status(500).json({ message: error.message });
+//       }
+//     }
+//   });
+// });
 
 // Rota para atualizar uma moeda
 router.put("/moedas/:id", async (req, res) => {
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,11 @@ const conectDatabase = require("./database/db");
 const usuarioRouter = require("./routes/rotasUser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const redis = require("redis");
+const client = redis.createClient();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 // Configurar o CORS com opções específicas
 app.use(
@@ -19,6 +20,19 @@ app.use(
 
 app.use(usuarioRouter);
 app.use(express.json());
+
+client.on("connect", () => {
+  console.log("Conectado ao Redis");
+});
+
+client.on("error", (err) => {
+  console.error("Erro de conexão com o Redis:", err);
+});
+
+app.use((req, res, next) => {
+  req.redisClient = client; // Adiciona o cliente Redis ao objeto `req`
+  next();
+});
 
 conectDatabase();
 
